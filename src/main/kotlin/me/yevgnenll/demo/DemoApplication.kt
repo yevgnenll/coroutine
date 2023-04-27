dd2fbf176958dc206f4582556bc6d04a83f80e7f
seungkwon <ffwd-seungkwon@FFWD-SEUNGKWONui-MacBookPro.local>
2023-04-27 18:56:44 +0900

    backup

--- src/main/kotlin/me/yevgnenll/demo/DemoApplication.kt
+++ src/main/kotlin/me/yevgnenll/demo/DemoApplication.kt
@@ -2,7 +2,9 @@ package me.yevgnenll.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
+@EnableReactiveMongoRepositories
 @SpringBootApplication
 class DemoApplication
 
