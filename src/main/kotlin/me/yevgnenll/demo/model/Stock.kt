35ba725aeee71169d9ee8b8da1a3fe2169ddfda1
yevgnenll <yevgnenll@gmail.com>
2023-05-01 17:49:22 +0900

    test: 간단한 테스트 코드 추가

--- src/main/kotlin/me/yevgnenll/demo/model/Stock.kt
+++ src/main/kotlin/me/yevgnenll/demo/model/Stock.kt
@@ -1,11 +1,26 @@
 package me.yevgnenll.demo.model
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
 
 @Document
 data class Stock(
+    @Id
     var id: String?,
+
+    @Field
     val name: String,
+
+    @Field
     val open: Double,
+
+    @Field
     val close: Double,
+
+    @Field
+    val high: Double,
+
+    @Field
+    val low: Double,
 )
